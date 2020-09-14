0f2c8e711df14447db798fb91ffc63673d705cf4
d.arkhipenko <arhiden@list.ru>
2020-09-15 00:19:56 +0300

    project (HomeWork) 4  is done

---
+++ Java - 4.1/src/main1.java
@@ -0,0 +1,23 @@
+public class main1 {
+    public static void main(String[] args) {
+
+        Engine engineSportCar = new Engine("Nissan", "VG30DET", 255.0);
+        Engine lorryEngine = new Engine("Nissan", "ZD30", 150.0);
+
+        Lorry lorry = new Lorry("Nissan Cabstar","Грузовик",2515, lorryEngine,1656);
+
+        SportCar sportCar = new SportCar("Nissan GT-R","Sport car",1740, engineSportCar,  280);
+
+
+
+        lorry.start();
+        lorry.turnLeft();
+        sportCar.turnRight();
+        sportCar.stop();
+
+        sportCar.printInfo();
+        System.out.println(lorry.printInfo());
+        System.out.println(sportCar.printInfo());
+
+    }
+}
